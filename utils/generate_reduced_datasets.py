8f39bd5a313d4996d048ced17bcb3b524b5fdbbc
root <youremail@yourdomain.com>
2024-04-19 11:10:47 +0800

    修改bug

--- utils/generate_reduced_datasets.py
+++ utils/generate_reduced_datasets.py
@@ -1,25 +1,22 @@
 import pandas as pd
 
-botiot = pd.read_csv("/data176/privatecloud/data/autodl-container-1b164bbe69-af1a6422-storage/ids_data/NF-BoT-IoT-v2.csv")
-cicids = pd.read_csv("/data176/privatecloud/data/autodl-container-1b164bbe69-af1a6422-storage/ids_data/NF-CSE-CIC-IDS2018-v2.csv")
-nb15 = pd.read_csv("/data176/privatecloud/data/autodl-container-1b164bbe69-af1a6422-storage/ids_data/NF-UNSW-NB15-v2.csv")
-toniot = pd.read_csv("/data176/privatecloud/data/autodl-container-1b164bbe69-af1a6422-storage/ids_data/NF-ToN-IoT-v2.csv")
+
 
 toniot_dist = [
     ["xss", 48652],
     ["Benign", 54024],
     ["scanning", 21335],
-    ["DDoS", 40000],
-    ["DoS", 12665],
+    ["ddos", 40000],
+    ["dos", 12665],
     ["injection", 13686],
     ["password", 22557],
-    ["Backdoor", 352],
+    ["backdoor", 352],
     ["mitm", 124],
     ["ransomware", 65]
 ]
 
 botiot_dist = [
-    ["DDoS", 84552],
+    ["DDoS", 184552],
     ["DoS", 167004],
     ["Reconnaissance", 25907],
     ["Benign", 1285],
@@ -46,18 +43,22 @@ cicids_dist = [
     ["DoS", 4855],
     ["Brute Force", 1202],
     ["Bot", 1510],
-    ["injection", 8]
+    ["SQL Injection", 8]
 ]
 
 
+toniot = pd.read_csv("/data176/privatecloud/data/autodl-container-1b164bbe69-af1a6422-storage/ids_data/NF-ToN-IoT-v2.csv")
 print("> TON-IoT")
 for attack in toniot_dist:
     num_samples_attack = toniot[toniot['Attack'] == attack[0]].shape[0]
     q = num_samples_attack - attack[1]
+    print(q)
     print(">> Sampling TON-IoT ({}) - Num of samples {} | Required Samples {} | To be removed {}".format(attack[0], num_samples_attack, attack[1], q))
     toniot = toniot.drop(toniot[toniot['Attack'] == attack[0]].sample(n=q).index)
 toniot.to_csv("/data176/privatecloud/data/autodl-container-1b164bbe69-af1a6422-storage/reduced_ids_data/toniot_bench_dnn.csv", index=False)
 
+
+botiot = pd.read_csv("/data176/privatecloud/data/autodl-container-1b164bbe69-af1a6422-storage/ids_data/NF-BoT-IoT-v2.csv")
 print("> Bot-IoT")
 for attack in botiot_dist:
     num_samples_attack = botiot[botiot['Attack'] == attack[0]].shape[0]
@@ -66,6 +67,8 @@ for attack in botiot_dist:
     botiot = botiot.drop(botiot[botiot['Attack'] == attack[0]].sample(n=q).index)
 botiot.to_csv("/data176/privatecloud/data/autodl-container-1b164bbe69-af1a6422-storage/reduced_ids_data/botiot_bench_dnn.csv", index=False)
 
+
+nb15 = pd.read_csv("/data176/privatecloud/data/autodl-container-1b164bbe69-af1a6422-storage/ids_data/NF-UNSW-NB15-v2.csv")
 print("> UNSW-NB15")
 for attack in nb15_dist:
     num_samples_attack = nb15[nb15['Attack'] == attack[0]].shape[0]
@@ -74,6 +77,39 @@ for attack in nb15_dist:
     nb15 = nb15.drop(nb15[nb15['Attack'] == attack[0]].sample(n=q).index)
 nb15.to_csv("/data176/privatecloud/data/autodl-container-1b164bbe69-af1a6422-storage/reduced_ids_data/nb15_bench_dnn.csv", index=False)
 
+
+
+cicids = pd.read_csv("/data176/privatecloud/data/autodl-container-1b164bbe69-af1a6422-storage/ids_data/NF-CSE-CIC-IDS2018-v2.csv")
+##处理NF-CSE-CIC-IDS2018-v2.csv,将细分的共计类别聚集到DoS,DDoS,Brute Force              
+ddos=cicids[cicids['Attack'].str.contains("ddos",case=False)]
+dos=cicids[cicids['Attack'].str.contains("dos",case=False)]
+cicids.drop(index=dos.index,inplace=True)
+dos.drop(index=ddos.index,inplace=True)
+
+brute=cicids[cicids['Attack'].str.contains("brute",case=False)]
+cicids.drop(index=brute.index,inplace=True)
+print(cicids.Attack.value_counts())
+print("before subsampling")
+print(dos.Attack.value_counts())
+print(ddos.Attack.value_counts())
+print(brute.Attack.value_counts())
+
+dos=pd.concat(objs=[dos])
+
+ddos=pd.concat(objs=[ddos])
+
+brute=pd.concat(objs=[brute])
+
+
+print("after subsampling")
+print(dos.Attack.value_counts())
+print(ddos.Attack.value_counts())
+print(brute.Attack.value_counts())
+
+dos.Attack="DoS"
+ddos.Attack="DDoS"
+brute.Attack="Brute Force"
+cicids=pd.concat(objs=[cicids, dos,ddos,brute])
 print("> CIC-IDS-2018")
 for attack in cicids_dist:
     num_samples_attack = cicids[cicids['Attack'] == attack[0]].shape[0]
